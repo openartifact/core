cc6b24c66a6f803847ff8b8a63104cef96a7b509
howardzchung@gmail.com <howardzchung@gmail.com>
2018-11-20 00:51:05 -0800

    add healthz, remove bins

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace openartifact_core.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/healthz")]
+        public string Login()
+        {
+            return "OK";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ openartifact-core
 Quickstart
 ====
 * Install the .NET Core SDK: https://www.microsoft.com/net/download
+  * On Linux (Ubuntu/Debian): `curl -sSL https://dot.net/v1/dotnet-install.sh | bash`
 * Install Visual Studio Code: https://code.visualstudio.com/
 * `dotnet watch run` to start the server and reload on save
 
